a61dde68cec79b0cc0e583d73b2a7bed17a3bf7f
DESKTOP-R0Q8TUC\hp <krasniqi@solution25.com>
2022-07-07 22:17:38 +0200

    Finishe the categori section

--- admin/functions.php
+++ admin/functions.php
@@ -26,8 +26,40 @@ function insert_categoris() {
 
 }
 
+function findeAllCategoris() {
+    global $connection;
+
+    // Finde all categories
+    $query = "SELECT * FROM categories LIMIT 3";
+    $select_categries = mysqli_query($connection, $query);
+    while ($row = mysqli_fetch_assoc($select_categries)) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+
+        echo "<tr>";
+        echo "<td>{$cat_id}</td>";
+        echo "<td>{$cat_title}</td>";
+        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+        echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+        echo "</tr>";
+    }
+}
 
 
+function deleteCategoris() {
+    global $connection;
+
+    if (isset($_GET['delete'])) {
+
+        $the_cat_id = $_GET['delete'];
+
+        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+
+        $delete_query = mysqli_query($connection, $query);
+
+        header("Location: categories.php");
+    }
+}
 
 
 
