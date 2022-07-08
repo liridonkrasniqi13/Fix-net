e5f977f553957fc30e8e2203416c5ecbaa49d047
DESKTOP-R0Q8TUC\hp <krasniqi@solution25.com>
2022-07-08 20:53:01 +0200

    add the html table for post

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -36,7 +36,7 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#posts"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="posts" class="collapse">
                             <li>
-                                <a href="#">View all Post</a>
+                                <a href="posts.php">View all Post</a>
                             </li>
                             <li>
                                 <a href="#">Add Post</a>
@@ -44,7 +44,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Category</a>
+                        <a href="categories.php"><i class="fa fa-fw fa-wrench"></i> Category</a>
                     </li>
                     
                     <li class="active">
---
+++ admin/posts.php
@@ -0,0 +1,70 @@
+<?php include "includes/header.php" ?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/navigation.php" ?>
+
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Blank Page
+                        <small>Subheading</small>
+                    </h1>
+
+                    
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Categories</th>
+                                    <th>Status</th>
+                                    <th>Images</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Id</td>
+                                    <td>Author</td>
+                                    <td>Title</td>
+                                    <td>Categories</td>
+                                    <td>Status</td>
+                                    <td>Images</td>
+                                    <td>Tags</td>
+                                    <td>Comments</td>
+                                    <td>Date</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    
+
+
+
+
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<?php include "includes/footer.php" ?>
