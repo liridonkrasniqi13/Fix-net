05c4015f5d2a8ef7e7b9ec660c5f92e9eec9cab1
DESKTOP-R0Q8TUC\hp <krasniqi@solution25.com>
2022-07-08 21:52:56 +0200

    Including Pages based on condition technique

--- admin/posts.php
+++ admin/posts.php
@@ -18,61 +18,37 @@
                         <small>Subheading</small>
                     </h1>
 
+                    <?php
+                    
+                        if(isset($_GET['source'])) {
 
-                    <div class="table-responsive">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Author</th>
-                                    <th>Title</th>
-                                    <th>Categories</th>
-                                    <th>Status</th>
-                                    <th>Images</th>
-                                    <th>Tags</th>
-                                    <th>Comments</th>
-                                    <th>Date</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                            $source = $_GET['source'];
 
-                                <?php
-                                
-                                $query = "SELECT * FROM posts ";
-                                $select_posts = mysqli_query($connection, $query);
-                                while ($row = mysqli_fetch_assoc($select_posts)) {
-                                    $post_id = $row['post_id'];
-                                    $post_title = $row['post_title'];
-                                    $post_author = $row['post_author'];
-                                    $post_date = $row['post_date'];
-                                    $post_image = $row['post_image'];
-                                    $post_content = $row['post_content'];
-                                    $post_comment_count = $row['post_comment_count'];
-                                    $post_status = $row['post_status'];
-                                    $post_tags = $row['post_tags'];
-                                    $post_category_id = $row['post_category_id'];
+                        } else {
+                            $source = '';
+                        }
 
-                                    echo "<tr>";
-                                    echo "<td>$post_id</td>";
-                                    echo "<td>$post_author</td>";
-                                    echo "<td>$post_title</td>";
-                                    echo "<td>$post_category_id</td>";
-                                    echo "<td>$post_status</td>";
-                                    echo "<td><img width='100' src='../img/$post_image'></td>";
-                                    echo "<td>$post_tags</td>";
-                                    echo "<td>$post_comment_count</td>";
-                                    echo "<td>$post_date</td>";
-                                    echo "</tr>";
+                        switch($source) {
+                            case '34';
+                            echo "nice 34";
+                            break;
 
-                                }
-                                
-                                
-                                ?>
+                            case '35';
+                            echo "nice 35";
+                            break;
+
+                            case '36';
+                            echo "nice 36";
+                            break;
 
 
-                            </tbody>
-                        </table>
-                    </div>
+                            default:
+                            include "includes/views_all_posts.php";
+                            break;
+                        }
+                    
+                    ?>
+                    
 
 
 
