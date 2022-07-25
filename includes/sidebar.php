f066be0770f2fa177b093e271d5c611a3dc2de65
DESKTOP-R0Q8TUC\hp <krasniqi@solution25.com>
2022-07-25 22:23:03 +0200

    Post Finished

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -36,8 +36,9 @@
                     <?php
                     while ($row = mysqli_fetch_assoc($select_categries_sidebar)) {
                         $cat_title = $row['cat_title'];
+                        $cat_id = $row['cat_id']; 
 
-                        echo "<li><a href='#'>{$cat_title}</a> </li>";
+                        echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a> </li>";
                     } ?>
 
                 </ul>
