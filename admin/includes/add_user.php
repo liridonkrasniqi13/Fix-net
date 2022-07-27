bcb2d406fcac89b6470853af09556f1da5b5cdcc
DESKTOP-R0Q8TUC\hp <krasniqi@solution25.com>
2022-07-27 21:40:05 +0200

    Add user html

---
+++ admin/includes/add_user.php
@@ -0,0 +1,94 @@
+<?php
+
+if (isset($_POST['create_post'])) {
+
+    $post_title = $_POST['title'];
+    $post_author = $_POST['author'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+    $post_date = date('d-m-y');
+
+
+    move_uploaded_file($post_image_temp, "../img/$post_image");
+
+    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
+
+    $query .= "VALUE('{$post_category_id}','{$post_title}','{$post_author}',now() ,'{'$post_image'}','{$post_content}','$post_tags','{$post_status}')";
+
+    $create_post_query = mysqli_query($connection, $query);
+
+    confirmQuery($create_post_query);
+
+}
+
+?>
+
+
+<form action="" method="post" enctype="multipart/form-data">
+    <legend>Add Post</legend>
+
+    <div class="form-group">
+        <label for="title">Post Title</label>
+        <input type="text" class="form-control" name="title" id="" placeholder="Post Title">
+    </div>
+
+    <div class="form-group">
+
+        <select name="post_category" id="post_category" class="form-control" required="required">
+
+            <?php
+
+            $query = "SELECT * FROM categories ";
+            $select_categries = mysqli_query($connection, $query);
+
+            // confirmQuery($select_categries);
+
+            while ($row = mysqli_fetch_assoc($select_categries)) {
+                $cat_id = $row['cat_id'];
+                $cat_title = $row['cat_title'];
+
+                echo "<option value='{$cat_id}'>{$cat_title}</option>";
+            }
+
+            ?>
+
+
+        </select>
+
+    </div>
+
+    <div class="form-group">
+        <label for="author">Post Author</label>
+        <input type="text" class="form-control" name="author" id="" placeholder="Post Author">
+    </div>
+
+    <div class="form-group">
+        <label for="post_status">Post Status</label>
+        <input type="text" class="form-control" name="post_status" id="" placeholder="Post Status">
+    </div>
+
+    <div class="form-group">
+        <label for="image">Post Image</label>
+        <input type="file" class="form-control" name="image" id="">
+    </div>
+
+    <div class="form-group">
+        <label for="post_tags">Post Tags</label>
+        <input type="text" class="form-control" name="post_tags" id="" placeholder="Post Tags">
+    </div>
+
+    <div class="form-group">
+        <label for="post_content">Post Content</label>
+        <textarea type="text" class="form-control" name="post_content" id="" placeholder="Post Content"></textarea>
+    </div>
+
+
+
+    <button type="submit" name="create_post" class="btn btn-primary">Submit</button>
+</form>
---
+++ admin/users.php
@@ -0,0 +1,70 @@
+<?php include "includes/header.php" ?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/navigation.php" ?>
+
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Blank Page
+                        <small>Subheading</small>
+                    </h1>
+
+                    <?php
+                    
+                        if(isset($_GET['source'])) {
+
+                            $source = $_GET['source'];
+
+                        } else {
+                            $source = '';
+                        }
+
+                        switch($source) {
+                            case 'add_post';
+                            include "includes/add_user.php";
+                            break;
+
+                            case 'edit_post';
+                            include "includes/edit_user.php";
+                            break;
+
+                            case '36';
+                            echo "nice 36";
+                            break;
+
+
+                            default:
+                            include "includes/views_all_users.php";
+                            break;
+                        }
+                    
+                    ?>
+                    
+
+
+
+
+
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<?php include "includes/footer.php" ?>
