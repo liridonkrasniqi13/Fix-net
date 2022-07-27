b8e0da6a17b70b55ed6c81bffabc75b8bf669134
DESKTOP-R0Q8TUC\hp <krasniqi@solution25.com>
2022-07-27 21:25:08 +0200

    published post

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
         <?php 
         
-        $query = "SELECT * FROM posts";
+        $query = "SELECT * FROM posts ";
         $select_all_post_query = mysqli_query($connection, $query);
 
         while($row = mysqli_fetch_assoc($select_all_post_query)) {
@@ -25,7 +25,14 @@
             $post_author = $row['post_author'];
             $post_date = $row['post_date'];
             $post_image = $row['post_image'];
-            $post_content = $row['post_content'];
+            $post_content = substr($row['post_content'],0,100);
+            $post_status = $row['post_status'];
+
+            if($post_status !== 'published') {
+                echo "<h1>There are no posts here</h1>";
+            } else {
+
+            
 
             ?>
 
@@ -52,7 +59,7 @@
 
 
             
-        <?php } ?>
+        <?php }  } ?>
         
         
 
