ff2d343208b394906fca87896de7a0208e87db3e
DESKTOP-R0Q8TUC\hp <krasniqi@solution25.com>
2022-12-04 11:49:39 +0100

    Fix exel 1

--- admin/export2.php
+++ admin/export2.php
@@ -1,6 +1,25 @@
-<?php include "includes/header.php"; ?>
 
 <?php
+
+
+$db['db_host'] = "localhost";
+$db['db_user'] = "krasniqi13";
+$db['db_pass'] = "Liverpool13";
+$db['db_name'] = "cms";
+
+foreach($db as $key => $value) {
+    define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+if($connection) {
+    
+    // echo "we are connected";
+
+}
+
+
 
 $output = '';
 
