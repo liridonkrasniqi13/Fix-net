c20f2538ad1738937a5f73f4e9df74c062525bf4
DESKTOP-R0Q8TUC\hp <krasniqi@solution25.com>
2022-12-06 20:30:53 +0100

    Change the icone

--- admin/icone.php
+++ admin/icone.php
@@ -7,18 +7,18 @@
                     <div class="media">
                         <div class="d-flex g-mr-15">
                             <div class="u-header-dropdown-icon-v1 g-pos-rel g-width-60 g-height-60 g-bg-lightblue-v3 g-font-size-18 g-font-size-24--md g-color-white rounded-circle">
-                                <i class="hs-admin-briefcase g-absolute-centered"></i>
+                                <i class="hs-admin-ticket g-absolute-centered"></i>
                             </div>
                         </div>
 
                         <div class="media-body align-self-center">
                             <div class="d-flex align-items-center g-mb-5">
                                 <span class="g-font-size-24 g-line-height-1 g-color-black">
-                                    <?php echo "<div class='huge'>{$post_count}</div>"; ?>
+                                    <?php echo "<div class='huge'>{$categories_status_coun_ticked}</div>"; ?>
                                 </span>
                             </div>
 
-                            <h6 class="g-font-size-16 g-font-weight-300 g-color-gray-dark-v6 mb-0">Intervenime</h6>
+                            <h6 class="g-font-size-16 g-font-weight-300 g-color-gray-dark-v6 mb-0">Tiket</h6>
                         </div>
                     </div>
                 </div>
@@ -33,19 +33,19 @@
                     <div class="media">
                         <div class="d-flex g-mr-15">
                             <div class="u-header-dropdown-icon-v1 g-pos-rel g-width-60 g-height-60 g-bg-teal-v2 g-font-size-18 g-font-size-24--md g-color-white rounded-circle">
-                                <i class="hs-admin-check-box g-absolute-centered"></i>
+                                <i class="hs-admin-layout-list-thumb-alt g-absolute-centered"></i>
                             </div>
                         </div>
 
                         <div class="media-body align-self-center">
                             <div class="d-flex align-items-center g-mb-5">
                                 <span class="g-font-size-24 g-line-height-1 g-color-black">
-                                    <?php echo "<div class='huge'>{$comments_count}</div>" ?>
+                                    <?php echo "<div class='huge'>{$categories_status_count}</div>" ?>
                                 </span>
 
                             </div>
 
-                            <h6 class="g-font-size-16 g-font-weight-300 g-color-gray-dark-v6 mb-0">Comments</h6>
+                            <h6 class="g-font-size-16 g-font-weight-300 g-color-gray-dark-v6 mb-0">Instalime</h6>
                         </div>
                     </div>
                 </div>
@@ -60,7 +60,7 @@
                     <div class="media">
                         <div class="d-flex g-mr-15">
                             <div class="u-header-dropdown-icon-v1 g-pos-rel g-width-60 g-height-60 g-bg-darkblue-v2 g-font-size-18 g-font-size-24--md g-color-white rounded-circle">
-                                <i class="hs-admin-wallet g-absolute-centered"></i>
+                                <i class="hs-admin-exchange-vertical g-absolute-centered"></i>
                             </div>
                         </div>
 
@@ -68,13 +68,13 @@
                             <div class="d-flex align-items-center g-mb-5">
                                 <span class="g-font-size-24 g-line-height-1 g-color-black">
                                     <?php
-                                    echo "<div class='huge'>{$users_count}</div>"
+                                    echo "<div class='huge'>{$categories_status_coun_rikyqje}</div>"
                                     ?>
                                 </span>
 
                             </div>
 
-                            <h6 class="g-font-size-16 g-font-weight-300 g-color-gray-dark-v6 mb-0">Users</h6>
+                            <h6 class="g-font-size-16 g-font-weight-300 g-color-gray-dark-v6 mb-0">Rikyqje</h6>
                         </div>
                     </div>
                 </div>
@@ -89,20 +89,48 @@
                     <div class="media">
                         <div class="d-flex g-mr-15">
                             <div class="u-header-dropdown-icon-v1 g-pos-rel g-width-60 g-height-60 g-bg-lightred-v2 g-font-size-18 g-font-size-24--md g-color-white rounded-circle">
-                                <i class="hs-admin-user g-absolute-centered"></i>
+                                <i class="hs-admin-layout-media-center-alt g-absolute-centered"></i>
                             </div>
                         </div>
 
                         <div class="media-body align-self-center">
                             <div class="d-flex align-items-center g-mb-5">
                                 <span class="g-font-size-24 g-line-height-1 g-color-black">
-                                    <?php echo "<div class='huge'>{$categories_count}</div>" ?>
+                                    <?php echo "<div class='huge'>{$categories_status_coun_catv}</div>" ?>
                                 </span>
 
                                 </span>
                             </div>
 
-                            <h6 class="g-font-size-16 g-font-weight-300 g-color-gray-dark-v6 mb-0">Categories</h6>
+                            <h6 class="g-font-size-16 g-font-weight-300 g-color-gray-dark-v6 mb-0">Catv2</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Panel -->
+        </div>
+
+        <div class="col-sm-6 col-lg-6 col-xl-3 g-mb-30">
+            <!-- Panel -->
+            <div class="card h-100 g-brd-gray-light-v7 g-rounded-3">
+                <div class="card-block g-font-weight-300 g-pa-20">
+                    <div class="media">
+                        <div class="d-flex g-mr-15">
+                            <div class="u-header-dropdown-icon-v1 g-pos-rel g-width-60 g-height-60 g-bg-lightred-v2 g-font-size-18 g-font-size-24--md g-color-white rounded-circle">
+                                <i class="hs-admin-calendar g-absolute-centered"></i>
+                            </div>
+                        </div>
+
+                        <div class="media-body align-self-center">
+                            <div class="d-flex align-items-center g-mb-5">
+                                <span class="g-font-size-24 g-line-height-1 g-color-black">
+                                    <?php echo "<div class='huge'>{$categories_status_coun_transfer}</div>" ?>
+                                </span>
+
+                                </span>
+                            </div>
+
+                            <h6 class="g-font-size-16 g-font-weight-300 g-color-gray-dark-v6 mb-0">Transfer</h6>
                         </div>
                     </div>
                 </div>
